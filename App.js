38128d299753bf2dba738eb77709a8043ab44be7
tonnidewan <tonni1687@gmail.com>
2019-05-24 01:02:08 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './src/AppNavigator';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppNavigator/>
+    );
+
+
+  }
+}
+
+
